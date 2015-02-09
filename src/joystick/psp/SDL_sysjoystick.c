2d1478fd06b734197f9696a49cbb7c9fc180e1d8
Alexandra Cherdantseva <cherdantseva@avroragames.co>
2015-02-09 09:21:28 +0300

    update from original repo, android memory leaks fixed

--- src/joystick/psp/SDL_sysjoystick.c
+++ src/joystick/psp/SDL_sysjoystick.c
@@ -18,6 +18,9 @@
      misrepresented as being the original software.
   3. This notice may not be removed or altered from any source distribution.
 */
+#include "../../SDL_internal.h"
+
+#if SDL_JOYSTICK_PSP
 
 /* This is the PSP implementation of the SDL joystick API */
 #include <pspctrl.h>
@@ -261,6 +264,8 @@ SDL_JoystickGUID SDL_SYS_JoystickGetGUID(SDL_Joystick * joystick)
     SDL_memcpy( &guid, name, SDL_min( sizeof(guid), SDL_strlen( name ) ) );
     return guid;
 }
+
+#endif /* SDL_JOYSTICK_PSP */
 
 /* vim: ts=4 sw=4
  */
